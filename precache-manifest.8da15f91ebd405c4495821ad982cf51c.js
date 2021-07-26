63794f4a9f4e8224cfad275a4966cd4a9469b8c1
nipunagarwal4235 <nipunagarwal2002@gmail.com>
2021-07-27 02:38:40 +0530

    Updates

---
+++ precache-manifest.8da15f91ebd405c4495821ad982cf51c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6af982be8a4d06dc6770a339f7e73975",
+    "url": "/menu-react/index.html"
+  },
+  {
+    "revision": "cf763a413c86dfb26133",
+    "url": "/menu-react/static/css/main.99af19dc.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/menu-react/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/menu-react/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf763a413c86dfb26133",
+    "url": "/menu-react/static/js/main.d7eb9d78.chunk.js"
+  },
+  {
+    "revision": "c1bb9e3367e8dafe9539",
+    "url": "/menu-react/static/js/runtime-main.d91d291d.js"
+  }
+]);
